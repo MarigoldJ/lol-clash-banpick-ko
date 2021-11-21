2eb2830e070e3a751d781861707dcbf0e1247701
MarigoldJ <yusang0130@gmail.com>
2021-11-21 19:21:54 +0900

    update 1.1 (rev)

--- src/server.js
+++ src/server.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = "http://localhost:38080";
-// export const SERVER_URL = "https://lol-clash-banpick-ko-server.herokuapp.com";
+// export const SERVER_URL = "http://localhost:38080";
+export const SERVER_URL = "https://lol-clash-banpick-ko-server.herokuapp.com";
