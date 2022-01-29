f3341635a4e5f3893a623c39f33c2c9e73cde9b9
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-01-29 13:38:32 +0900

    [feat] 페이지 추가

---
+++ src/screens/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as LinkInfo } from "./LinkInfo";
+export { default as NotFound } from "./NotFound";
+export { default as TeamBan } from "./TeamBan";
