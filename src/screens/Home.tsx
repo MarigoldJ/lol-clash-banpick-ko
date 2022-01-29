8527e31100c0904bd9c3f28156814f55ab5d6f30
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-01-29 19:11:09 +0900

    [feat] Home 스크린에 Team 컴포넌트 추가

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,36 @@
+import styled from "styled-components";
+import { TeamForm } from "@components/Team";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <Container>
+      <div className="home-main">
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <TeamForm teamColor={"blue"} />
+        <TeamForm teamColor={"red"} />
+      </div>
+    </Container>
+  );
 }
 
 export default Home;
+
+// Style
+const Container = styled.div`
+  .home-main {
+    position: absolute;
+    left: 25%;
+    top: 25%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 50%;
+    height: 50%;
+
+    width: 50%;
+  }
+`;
