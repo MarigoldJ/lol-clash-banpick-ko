314d0b092d70047346bf213aabf193351f4f079e
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-02-18 18:52:32 +0900

    [feat] LinkInfo 페이지 기본 골격 생성

--- src/screens/LinkInfo/index.tsx
+++ src/screens/LinkInfo/index.tsx
@@ -1,5 +1,14 @@
+import { mainBgImg } from "@assets/image";
+import BasicLayout from "@components/BasicLayout";
+
 function LinkInfo() {
-  return <h1>LinkInfo</h1>;
+  return (
+    <BasicLayout bgImg={mainBgImg}>
+      <div>blue team link</div>
+      <div>red team link</div>
+      <div>observer link</div>
+    </BasicLayout>
+  );
 }
 
 export default LinkInfo;
