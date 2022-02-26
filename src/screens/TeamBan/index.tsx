73d4f30351a24bc1888137c138ddb38cfba2346d
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-02-27 00:40:44 +0900

    [feat] 챔피언 리스트 받아오는 request 작성

--- src/screens/TeamBan/index.tsx
+++ src/screens/TeamBan/index.tsx
@@ -1,13 +1,45 @@
 import { TextField } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 function TeamBan() {
+  const [champList, setChampLIst] = useState<Array<Object>>([]);
   const [champName, setChampName] = useState<string>("");
   const handleChampName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setChampName(event.target.value);
   };
 
+  // TODO: 이후 제거할 코드. 디버깅용.
+  console.log(champList);
+
+  useEffect(() => {
+    if (champList.length === 0) {
+      const requestURL =
+        "https://ddragon.leagueoflegends.com/cdn/11.23.1/data/ko_KR/champion.json";
+      const request = new XMLHttpRequest();
+      request.open("GET", requestURL);
+      request.responseType = "json";
+      request.send();
+
+      request.onload = () => {
+        console.log("Champion loaded!");
+        const { data }: { data: Array<Object> } = request.response;
+        const chlist: Array<Object> = Object.values(data).sort(
+          (a: any, b: any) => {
+            if (a.name < b.name) {
+              return -1;
+            } else if (a.name > b.name) {
+              return 1;
+            } else {
+              return 0;
+            }
+          }
+        );
+
+        setChampLIst(chlist);
+      };
+    }
+  }, [champList]);
   return (
     <div>
       <Content
