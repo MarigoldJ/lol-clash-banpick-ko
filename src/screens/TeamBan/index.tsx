a8741475661523596c6c4a1cd0b542f0c7edbe9b
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-02-27 00:38:45 +0900

    [feat] 챔피언명 검색창 추가

--- src/screens/TeamBan/index.tsx
+++ src/screens/TeamBan/index.tsx
@@ -1,5 +1,51 @@
+import { TextField } from "@mui/material";
+import React, { useState } from "react";
+import styled from "styled-components";
+
 function TeamBan() {
-  return <h1>TeamBan</h1>;
+  const [champName, setChampName] = useState<string>("");
+  const handleChampName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChampName(event.target.value);
+  };
+
+  return (
+    <div>
+      <Content
+        id="champion-search-box"
+        label="챔피언 명"
+        value={champName}
+        onChange={handleChampName}
+      />
+    </div>
+  );
 }
 
 export default TeamBan;
+
+const Content = styled(TextField)`
+  & label.Mui-focused {
+    color: white;
+  }
+  & label {
+    color: gray;
+  }
+
+  & .MuiInput-underline:after {
+    border-bottom-color: white;
+  }
+  & .MuiOutlinedInput-root {
+    & fieldset {
+      border-color: gray;
+    }
+    &:hover fieldset {
+      border-color: white;
+    }
+    &.Mui-focused fieldset {
+      border-color: white;
+    }
+
+    & input {
+      color: white;
+    }
+  }
+`;
