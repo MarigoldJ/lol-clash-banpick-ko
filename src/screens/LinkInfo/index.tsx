06e76281e7e51dab2b87055a96416bbe90d61f06
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-02-27 18:09:35 +0900

    [feat] Home페이지에서 서버로부터의 응답 넘겨받아 출력

--- src/screens/LinkInfo/index.tsx
+++ src/screens/LinkInfo/index.tsx
@@ -1,28 +1,65 @@
 import { mainBgImg } from "@assets/image";
 import BasicLayout from "@components/BasicLayout";
 import TeamSide from "@components/TeamSide";
-import { LinkCopyBtnContainer } from "./components";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { ErrorMsg, LinkCopyBtnContainer } from "./components";
+
+type IBanpickInfo = {
+  teamName: {
+    blueName: string;
+    redName: string;
+  };
+  link: {
+    blue: string;
+    red: string;
+    obs: string;
+  };
+};
 
 function LinkInfo() {
-  // TODO: 팀 이름은 Home 페이지에서 넘겨받기
-  const teamName = { blueName: "블루블루", redName: "레드레드" };
+  const [banpickInfo, setBanpickInfo] = useState<IBanpickInfo>({
+    teamName: { blueName: "", redName: "" },
+    link: { blue: "", red: "", obs: "" },
+  });
 
-  // TODO: 밴픽 링크 서버에서 받아오기
-  const banpickLink = {
-    blue: "blue team link",
-    red: "red team link",
-    obs: "observer Link",
-  };
+  const [isWrong, setIsWrong] = useState<boolean>(false);
+  const { state }: { state: any } = useLocation();
+
+  useEffect(() => {
+    if (!state) {
+      setIsWrong(true);
+    } else {
+      // Home페이지에서 얻어온 state에서 밴픽 링크 얻기
+      const { origin } = new URL(document.location.href);
+      setBanpickInfo({
+        teamName: {
+          blueName: state.blue?.name,
+          redName: state.red?.name,
+        },
+        link: {
+          blue: origin + state.blue?.pathname,
+          red: origin + state.red?.pathname,
+          obs: origin + state.observer?.pathname,
+        },
+      });
+    }
+  }, [state]);
 
   return (
     <BasicLayout bgImg={mainBgImg}>
-      <LinkCopyBtnContainer link={banpickLink.blue}>
-        <TeamSide side="blue">{`팀:${teamName.blueName}`}</TeamSide>
+      {isWrong && (
+        <ErrorMsg>
+          잘못된 링크로 접근하셨습니다. 팀 이름 입력 후 밴픽링크를 생성해주세요.
+        </ErrorMsg>
+      )}
+      <LinkCopyBtnContainer link={banpickInfo.link.blue}>
+        <TeamSide side="blue">{`팀:${banpickInfo.teamName.blueName}`}</TeamSide>
       </LinkCopyBtnContainer>
-      <LinkCopyBtnContainer link={banpickLink.red}>
-        <TeamSide side="red">{`팀:${teamName.redName}`}</TeamSide>
+      <LinkCopyBtnContainer link={banpickInfo.link.red}>
+        <TeamSide side="red">{`팀:${banpickInfo.teamName.redName}`}</TeamSide>
       </LinkCopyBtnContainer>
-      <LinkCopyBtnContainer link={banpickLink.obs}>
+      <LinkCopyBtnContainer link={banpickInfo.link.obs}>
         <TeamSide side="none">{"관전자"}</TeamSide>
       </LinkCopyBtnContainer>
     </BasicLayout>
