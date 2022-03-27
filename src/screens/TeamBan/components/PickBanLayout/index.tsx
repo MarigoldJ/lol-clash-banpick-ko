3fca775947ce617a8c6923dffc0b7edbd567d57c
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-03-27 23:07:02 +0900

    [feat] PickBanLayout 구조 설정(양쪽 밴픽창, 가운데 children)

--- src/screens/TeamBan/components/PickBanLayout/index.tsx
+++ src/screens/TeamBan/components/PickBanLayout/index.tsx
@@ -6,10 +6,45 @@ type IProps = {
 };
 
 function PickBanLayout({ children }: IProps) {
-  return <Container>{children}</Container>;
+  return (
+    <Container>
+      <div className="side">
+        <div className="windows-pick"></div>
+        <div className="windows-ban"></div>
+      </div>
+
+      <div className="center">{children}</div>
+
+      <div className="side">
+        <div className="windows-pick"></div>
+        <div className="windows-ban"></div>
+      </div>
+    </Container>
+  );
 }
 
 export default PickBanLayout;
 
 // Style
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+
+  .side {
+    flex: 2;
+    background-color: rgb(30, 30, 30);
+
+    .windows-pick {
+      width: 100%;
+    }
+
+    .windows-ban {
+      width: 100%;
+    }
+  }
+  .center {
+    flex: 5;
+  }
+`;
