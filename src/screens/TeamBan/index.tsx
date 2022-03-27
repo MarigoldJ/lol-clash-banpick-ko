8c7e49037f9b902512fddee9a7b53a5f6f46eb28
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-03-27 23:33:15 +0900

    [feat] 롤 패치버전, 챔피언리스트 불러오는 request 추가

--- src/screens/TeamBan/index.tsx
+++ src/screens/TeamBan/index.tsx
@@ -1,5 +1,6 @@
 import { fakeBanpickInfo } from "@utils/general";
 import { ChampData, ClientBanpickData } from "@utils/type";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import PickBanHeader from "./components/PickBanHeader";
 import PickBanLayout from "./components/PickBanLayout";
@@ -7,9 +8,54 @@ import SelectUI from "./components/SelectUI";
 import GameContext from "./contexts/GameContext";
 
 function TeamBan() {
+  //   게임 기본 데이터(버전, 챔피언 리스트) 불러오기
+  const [gameVersion, setGameVersion] = useState("");
+  const [champList, setChampList] = useState<Array<ChampData>>([]);
+  useEffect(() => {
+    if (gameVersion === "") {
+      // 패치 버전이 없는 경우 패치버전부터 불러오기
+      const requestURL =
+        "https://ddragon.leagueoflegends.com/api/versions.json";
+      const request = new XMLHttpRequest();
+      request.open("GET", requestURL);
+      request.responseType = "json";
+      request.send();
+
+      request.onload = () => {
+        console.log("LOL Version loaded!");
+        const data = request.response;
+        setGameVersion(data[0]);
+      };
+    } else {
+      // 패치 버전이 있는 경우 챔피언 리스트 불러오기
+      const requestURL = `https://ddragon.leagueoflegends.com/cdn/${gameVersion}/data/ko_KR/champion.json`;
+      const request = new XMLHttpRequest();
+      request.open("GET", requestURL);
+      request.responseType = "json";
+      request.send();
+
+      request.onload = () => {
+        console.log("Champion loaded!");
+        const { data }: { data: Array<ChampData> } = request.response;
+        const chlist: Array<ChampData> = Object.values(data).sort(
+          (a: any, b: any) => {
+            if (a.name < b.name) {
+              return -1;
+            } else if (a.name > b.name) {
+              return 1;
+            } else {
+              return 0;
+            }
+          }
+        );
+
+        setChampList(chlist);
+      };
+    }
+  }, [gameVersion]);
+
   // TODO: 서버에서 받아온 BanpickInfo로 대체할 것
   const banpickInfo: ClientBanpickData = fakeBanpickInfo;
-  const champList: Array<ChampData> = [];
 
   return (
     <GameContext.Provider value={{ banpickInfo, champList }}>
