073bcc6435121f9b1161d4244680a8f668f73a5e
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-03-27 22:45:03 +0900

    [feat] 서버에서 정보를 받아오기 전 가짜 banpickInfo 변수 추가

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,4 +1,4 @@
-import { ChampData } from ".";
+import { ChampData, ClientBanpickData } from "@utils/type";
 
 type IProps = {
   type: "splash" | "loading" | "square";
@@ -21,3 +21,32 @@ export function getLOLImgUrl({ type, champ }: IProps) {
   // champ === undefined
   return "";
 }
+
+export const fakeBanpickInfo: ClientBanpickData = {
+  isBlueReady: false,
+  isRedReady: false,
+  phase: 1,
+  startTime: Date.now(),
+  blueName: "",
+  redName: "",
+  1: "",
+  2: "",
+  3: "",
+  4: "",
+  5: "",
+  6: "",
+  7: "",
+  8: "",
+  9: "",
+  10: "",
+  11: "",
+  12: "",
+  13: "",
+  14: "",
+  15: "",
+  16: "",
+  17: "",
+  18: "",
+  19: "",
+  20: "",
+};
