ee6a3e0ed1030ba22476c2f9b5816d519056acd8
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-03-27 22:48:09 +0900

    [feat] TeamBan 페이지 상단의 Header(팀명, 타이머) 추가

--- src/screens/TeamBan/components/PickBanHeader/index.tsx
+++ src/screens/TeamBan/components/PickBanHeader/index.tsx
@@ -1,10 +1,32 @@
+import TeamSide from "@components/TeamSide";
 import styled from "styled-components";
 
-function PickBanHeader() {
-  return <Container></Container>;
+type IProps = {
+  blueName: string;
+  redName: string;
+};
+
+function PickBanHeader({ blueName, redName }: IProps) {
+  return (
+    <Container>
+      <TeamSide side="blue">{blueName}</TeamSide>
+      <div className="timer">:00</div>
+      <TeamSide side="red">{redName}</TeamSide>
+    </Container>
+  );
 }
 
 export default PickBanHeader;
 
 // Style
-const Container = styled.div``;
+const Container = styled.div`
+  padding: 10px 30px;
+
+  width: 100%;
+  height: 80px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
--- src/screens/TeamBan/index.tsx
+++ src/screens/TeamBan/index.tsx
@@ -1,12 +1,20 @@
+import { fakeBanpickInfo } from "@utils/general";
+import { ClientBanpickData } from "@utils/type";
 import styled from "styled-components";
 import PickBanHeader from "./components/PickBanHeader";
 import PickBanLayout from "./components/PickBanLayout";
 import SelectUI from "./components/SelectUI";
 
 function TeamBan() {
+  // TODO: 서버에서 받아온 BanpickInfo로 대체할 것
+  const banpickInfo: ClientBanpickData = fakeBanpickInfo;
+
   return (
     <Container>
-      <PickBanHeader />
+      <PickBanHeader
+        blueName={banpickInfo.blueName}
+        redName={banpickInfo.redName}
+      />
       <PickBanLayout>
         <SelectUI />
       </PickBanLayout>
