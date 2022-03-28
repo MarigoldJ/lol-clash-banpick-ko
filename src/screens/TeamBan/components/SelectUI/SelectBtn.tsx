da6c04caf26faa9b1effa7742cc4fcf533d0385f
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-03-29 02:45:16 +0900

    [feat] 자신의 차례에만 선택완료버튼 보이도록 추가

--- src/screens/TeamBan/components/SelectUI/SelectBtn.tsx
+++ src/screens/TeamBan/components/SelectUI/SelectBtn.tsx
@@ -1,6 +1,8 @@
 import GameContext from "@screens/TeamBan/contexts/GameContext";
 import fonts from "@styles/fonts";
+import { getParamFromQueryStr } from "@utils/general";
 import { useContext } from "react";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 
 function SelectBtn() {
@@ -21,11 +23,24 @@ function SelectBtn() {
     });
   };
 
-  return (
-    <SButton onClick={clickBtn} disabled={!selectedChampId}>
-      선택 완료
-    </SButton>
-  );
+  // url에서 어느 팀인지 따오기
+  const { search } = useLocation();
+  const teamSide = getParamFromQueryStr(search, "team");
+
+  // 현재 phase가 자신의 차례인지 여부
+  const isBlueTurn = [1, 3, 5, 7, 10, 11, 18, 19].includes(banpickInfo.phase);
+  const isMyTurn = isBlueTurn ? teamSide === "blue" : teamSide === "red";
+
+  // 자신의 차례일때만 선택완료 버튼 반환
+  if (isMyTurn) {
+    return (
+      <SButton onClick={clickBtn} disabled={!selectedChampId}>
+        선택 완료
+      </SButton>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default SelectBtn;
