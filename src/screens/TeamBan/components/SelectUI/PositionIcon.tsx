f6076189201a9835414603160bf320bd7cccc3b9
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-04-01 01:48:05 +0900

    [refactor] position 종류를 type으로 추가

--- src/screens/TeamBan/components/SelectUI/PositionIcon.tsx
+++ src/screens/TeamBan/components/SelectUI/PositionIcon.tsx
@@ -1,11 +1,13 @@
+import { Position } from "@utils/type";
 import styled from "styled-components";
 
 type IProps = {
-  position: "TOP" | "JUNGLE" | "MIDDLE" | "ADC" | "SUPPORT";
+  position: Position;
 };
 
 export default function PositionIcon({ position }: IProps) {
   const imgUrl = `https://raw.githubusercontent.com/esports-bits/lol_images/master/role_lane_icons/${position}.png`;
+
   return <StyledImg src={imgUrl} alt={position} />;
 }
 
