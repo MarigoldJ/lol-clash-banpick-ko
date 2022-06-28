de1d4531ae9786fc962a2a8ae5540bb02ae625fb
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-06-28 17:15:12 +0900

    [fix] 밴 2페이즈에서 블루팀에게 선택완료버튼 뜨도록 수정

--- src/screens/TeamBan/components/SelectUI/SelectBtn.tsx
+++ src/screens/TeamBan/components/SelectUI/SelectBtn.tsx
@@ -28,7 +28,9 @@ function SelectBtn() {
   const teamSide = getParamFromQueryStr(search, "team");
 
   // 현재 phase가 자신의 차례인지 여부
-  const isBlueTurn = [1, 3, 5, 7, 10, 11, 18, 19].includes(banpickInfo.phase);
+  const isBlueTurn = [1, 3, 5, 7, 10, 11, 14, 16, 18, 19].includes(
+    banpickInfo.phase
+  );
   const isMyTurn = isBlueTurn ? teamSide === "blue" : teamSide === "red";
 
   // 자신의 차례일때만 선택완료 버튼 반환
